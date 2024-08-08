d79a7c7ef75c15933293fa632a75667e0e5dbd8e
Roja503 <146074998+Roja503@users.noreply.github.com>
2024-08-08 12:23:44 +0530

    Create area of circle.java

---
+++ area of circle.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        {
+         System.out.println("area"+(3.14*r*r)); 
+        }
+        
+        
+    
+    }
+}
